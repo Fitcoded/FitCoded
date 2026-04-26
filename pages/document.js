f1efbc30496ac7280178a53d81360e3364ead99e
Fitcoded <Junicue@me.com>
2026-04-25 22:34:23 -0400

    Create document.js

---
+++ pages/document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://s.skimresources.com/js/302110X1790099.skimlinks.js"
+          async
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
